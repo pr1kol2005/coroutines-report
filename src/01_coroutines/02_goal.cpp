0b06f07cab72fc1021b8f32eb2548397cec85e79
cyril grinko <k.a.grinko@gmail.com>
2025-05-21 12:09:51 +0300

    goal update

--- src/01_coroutines/02_goal.cpp
+++ src/01_coroutines/02_goal.cpp
@@ -6,7 +6,8 @@ struct Resumable {
   struct promise_type;
   using handle_type = std::coroutine_handle<promise_type>;
 
-  Resumable(handle_type h) : handle_(h) {}
+  Resumable(handle_type handle) : handle_(handle) {}
+
   ~Resumable() {
     if (handle_) {
       handle_.destroy();
@@ -15,13 +16,17 @@ struct Resumable {
 
   Resumable(const Resumable&) = delete;
   Resumable& operator=(const Resumable&) = delete;
+
   Resumable(Resumable&& other) : handle_(other.handle_) { other.handle_ = {}; }
+
   Resumable& operator=(Resumable&& other) {
-    if (handle_) {
-      handle_.destroy();
+    if (this != std::addressof(other)) {
+      if (handle_) {
+        handle_.destroy();
+      }
+      handle_ = other.handle_;
+      other.handle_ = {};
     }
-    handle_ = other.handle_;
-    other.handle_ = {};
     return *this;
   }
 
@@ -39,10 +44,19 @@ struct Resumable::promise_type {
   Resumable get_return_object() {
     return Resumable(handle_type::from_promise(*this));
   }
+
   // TODO : show suspend_always / suspend_never difference
-  std::suspend_always initial_suspend() { return {}; }
+  std::suspend_always initial_suspend() noexcept { return {}; }
+
   std::suspend_always final_suspend() noexcept { return {}; }
+
+  // TODO : need for co_yield
+  // std::suspend_always yield_value(T value) noexcept { }
+
   void unhandled_exception() { std::terminate(); }
+
+  // TODO : ??
+  void return_void() noexcept {}
 };
 
 Resumable foo() {
